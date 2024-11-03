dbfe97b0f9a123e4b12ab61a194aec57a790676b
m-alkordy <moda-233@hotmail.com>
2024-11-03 21:26:25 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import NavBar from './components/NavBar/NavBar'
 
 
 function App() {
-  const items = ["Home" , "About" , "Service" , "New Property" , "Contact"]
+  const items = ["Home" , "About" , "Service" , "New Property" , "Contact" , "from alaa"]
   return (
     <>
-    <NavBar logo= "assets/img/logo.png" items={items} btn="Login" />
+    <NavBar logo= "assets/img/logos.png" items={items} btn="Login test" />
     <h1>hello my friend I'm alaa</h1>
     </>
   )
